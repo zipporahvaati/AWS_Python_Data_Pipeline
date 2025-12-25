0a092536737dec75997750f0594efbb9415b97cd
Zipporah Vaati <zipporahvaati@users.noreply.github.com>
2025-12-25 18:20:46 +0300

    Update read_s3.py

--- read_s3.py
+++ read_s3.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Connect to S3
 s3 = boto3.client('s3')
 bucket = 'zippy-vaati-s3-bucket'  # bucket name
-file_key = 'BMW Sales.csv'      # your file name
+file_key = 'BMW Sales.csv'      # file name
 
 # Read CSV into pandas
 obj = s3.get_object(Bucket=bucket, Key=file_key)
